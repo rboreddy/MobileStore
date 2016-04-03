7fcf0f018fab2fd4070d4bff46b2383f1b7dd3ba
boreddy.ravi <boreddy.ravi@gmail.com>
2016-04-03 17:51:20 -0400

    change

--- mymobilestore/src/main/java/mymobilestore/HelloWorld.java
+++ mymobilestore/src/main/java/mymobilestore/HelloWorld.java
@@ -2,9 +2,8 @@ package mymobilestore;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String sayHelloWorld(){
+		return "Hello Ravi!";
 	}
 
 }
