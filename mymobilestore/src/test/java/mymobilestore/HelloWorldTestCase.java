b9d0c1e2048943924bed4f2ac0210b42eb7f23a9
boreddy.ravi <boreddy.ravi@gmail.com>
2016-04-03 18:03:35 -0400

    unit test file check-in

---
+++ mymobilestore/src/test/java/mymobilestore/HelloWorldTestCase.java
@@ -0,0 +1,16 @@
+package mymobilestore;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTestCase {
+	
+	
+	private HelloWorld helloWorld = new HelloWorld();
+
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello World Ravi!", helloWorld.sayHelloWorld());
+	}
+
+}
